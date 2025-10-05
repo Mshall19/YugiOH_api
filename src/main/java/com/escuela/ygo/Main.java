e26a48cb8e19e0f85d43d359cfa672ab228ce34f
Juan David Perez <112208242+Mshall19@users.noreply.github.com>
2025-10-04 19:47:57 -0500

    Delete src/main/java/com/escuela/ygo directory

--- src/main/java/com/escuela/ygo/Main.java
+++
@@ -1,9 +0,0 @@
-package com.escuela.ygo;
-
-import com.escuela.ygo.gui.VentanaDuelo;
-
-public class Main {
-    public static void main(String[] args) {
-        VentanaDuelo.lanzar();
-    }
-}
--- src/main/java/com/escuela/ygo/batalla/Batalla.java
+++
@@ -1,7 +0,0 @@
-package com.escuela.ygo.batalla;
-
-public interface Batalla {
-    void alIniciarTurno(String cartaJugador, String cartaIA, String ganador);
-    void alCambiarPuntaje(int puntajeJugador, int puntajeIA);
-    void alFinalizarDuelo(String ganador);
-}
